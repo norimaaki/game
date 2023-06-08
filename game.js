5048e0857c9d4f96168cdbe8db13a22a6bf855b9
norimaaki <134594111+norimaaki@users.noreply.github.com>
2023-06-08 18:15:24 +0900

    Update game.js

--- game.js
+++ game.js
@@ -6,7 +6,20 @@ const ctx = canvas.getContext('2d');
 
 // プレイヤーの初期位置
 let playerX = canvas.width / 2;
-let playerY = canvas.height / 2;
+let playerY = canvas.height - 50;
+const playerSize = 50;
+
+// スタートとゴールの位置
+const startX = 50;
+const startY = canvas.height - 100;
+const goalX = canvas.width - 100;
+const goalY = 50;
+const goalSize = 50;
+
+// 障害物の初期位置
+let obstacleX = 200;
+let obstacleY = 100;
+const obstacleSize = 50;
 
 // タッチの状態を格納するオブジェクト
 const touches = {};
@@ -47,13 +60,51 @@ function handleTouchMove(event) {
   }
 }
 
+// 衝突判定
+function checkCollision() {
+  if (
+    playerX < obstacleX + obstacleSize &&
+    playerX + playerSize > obstacleX &&
+    playerY < obstacleY + obstacleSize &&
+    playerY + playerSize > obstacleY
+  ) {
+    return true; // 衝突した
+  }
+  return false; // 衝突していない
+}
+
+// ゲームクリア判定
+function checkGameClear() {
+  if (
+    playerX < goalX + goalSize &&
+    playerX + playerSize > goalX &&
+    playerY < goalY + goalSize &&
+    playerY + playerSize > goalY
+  ) {
+    return true; // ゲームクリア
+  }
+  return false; // ゲームクリアしていない
+}
+
 // ゲームループ
 function gameLoop() {
   // キャンバスをクリア
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   // プレイヤーを描画
-  ctx.fillRect(playerX, playerY, 50, 50);
+  ctx.fillRect(playerX, playerY, playerSize, playerSize);
+
+  // スタートを描画
+  ctx.fillStyle = 'green';
+  ctx.fillRect(startX, startY, playerSize, playerSize);
+
+  // ゴールを描画
+  ctx.fillStyle = 'red';
+  ctx.fillRect(goalX, goalY, goalSize, goalSize);
+
+  // 障害物を描画
+  ctx.fillStyle = 'gray';
+  ctx.fillRect(obstacleX, obstacleY, obstacleSize, obstacleSize);
 
   // タッチの入力に応じてプレイヤーの位置を更新
   for (let touchId in touches) {
@@ -61,19 +112,36 @@ function gameLoop() {
     if (touch.x < playerX && playerX > 0) {
       playerX -= 5;
     }
-    if (touch.x > playerX && playerX < canvas.width - 50) {
+    if (touch.x > playerX && playerX < canvas.width - playerSize) {
       playerX += 5;
     }
     if (touch.y < playerY && playerY > 0) {
       playerY -= 5;
     }
-    if (touch.y > playerY && playerY < canvas.height - 50) {
+    if (touch.y > playerY && playerY < canvas.height - playerSize) {
       playerY += 5;
     }
   }
 
-  // ループを継続するために再帰呼び出し
-  requestAnimationFrame(gameLoop);
+  // 衝突判定
+  if (checkCollision()) {
+    // ゲームオーバー処理
+    alert('ゲームオーバー');
+    resetGame();
+  } else if (checkGameClear()) {
+    // ゲームクリア処理
+    alert('ゲームクリア！');
+    resetGame();
+  } else {
+    // ループを継続するために再帰呼び出し
+    requestAnimationFrame(gameLoop);
+  }
+}
+
+// ゲームをリセット
+function resetGame() {
+  playerX = startX;
+  playerY = startY;
 }
 
 // ゲームループを開始
