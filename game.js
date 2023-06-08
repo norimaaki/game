2620c7968ec159e3e04a6a55420f837950d82eae
norimaaki <134594111+norimaaki@users.noreply.github.com>
2023-06-08 20:09:32 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,148 +1,171 @@
 // ゲームのロジックを記述する
 
-// キャンバス要素を取得
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-// プレイヤーの初期位置
-let playerX = canvas.width / 2;
-let playerY = canvas.height - 50;
-const playerSize = 50;
+const mario = {
+  x: 50,
+  y: 0,
+  width: 50,
+  height: 50,
+  speed: 5,
+  velocityY: 0,
+  jumping: false
+};
 
-// スタートとゴールの位置
-const startX = 50;
-const startY = canvas.height - 100;
-const goalX = canvas.width - 100;
-const goalY = 50;
-const goalSize = 50;
+const gravity = 0.5;
+const platform = {
+  x: 0,
+  y: canvas.height - 50,
+  width: canvas.width,
+  height: 50
+};
 
-// 障害物の初期位置
-let obstacleX = 200;
-let obstacleY = 100;
-const obstacleSize = 50;
+const enemy = {
+  x: canvas.width - 50,
+  y: canvas.height - 50,
+  width: 50,
+  height: 50,
+  speed: 3
+};
 
-// タッチの状態を格納するオブジェクト
-const touches = {};
+const coin = {
+  x: canvas.width / 2,
+  y: canvas.height - 100,
+  width: 25,
+  height: 25,
+  collected: false
+};
 
-// タッチイベントのリスナーを設定
-canvas.addEventListener('touchstart', handleTouchStart);
-canvas.addEventListener('touchend', handleTouchEnd);
-canvas.addEventListener('touchcancel', handleTouchEnd);
-canvas.addEventListener('touchmove', handleTouchMove);
+const goal = {
+  x: canvas.width - 100,
+  y: 0,
+  width: 100,
+  height: 50
+};
 
-// タッチが開始されたときの処理
-function handleTouchStart(event) {
-  event.preventDefault();
-  for (let touch of event.changedTouches) {
-    touches[touch.identifier] = { x: touch.clientX, y: touch.clientY };
+function drawMario() {
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(mario.x, mario.y, mario.width, mario.height);
+}
+
+function drawPlatform() {
+  ctx.fillStyle = 'green';
+  ctx.fillRect(platform.x, platform.y, platform.width, platform.height);
+}
+
+function drawEnemy() {
+  ctx.fillStyle = 'red';
+  ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+}
+
+function drawCoin() {
+  if (!coin.collected) {
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(coin.x, coin.y, coin.width, coin.height);
   }
 }
 
-// タッチが終了したときの処理
-function handleTouchEnd(event) {
-  event.preventDefault();
-  for (let touch of event.changedTouches) {
-    delete touches[touch.identifier];
+function drawGoal() {
+  ctx.fillStyle = 'orange';
+  ctx.fillRect(goal.x, goal.y, goal.width, goal.height);
+}
+
+function applyGravity() {
+  if (mario.y + mario.height < canvas.height) {
+    mario.velocityY += gravity;
+    mario.y += mario.velocityY;
+  } else {
+    mario.velocityY = 0;
+    mario.y = canvas.height - mario.height;
+    mario.jumping = false;
   }
 }
 
-// タッチがキャンセルされたときの処理
-function handleTouchCancel(event) {
-  event.preventDefault();
-  touches = {};
-}
-
-// タッチが移動したときの処理
-function handleTouchMove(event) {
-  event.preventDefault();
-  for (let touch of event.changedTouches) {
-    touches[touch.identifier] = { x: touch.clientX, y: touch.clientY };
+function handleJump() {
+  if (!mario.jumping) {
+    mario.velocityY = -12;
+    mario.jumping = true;
   }
 }
 
-// 衝突判定
-function checkCollision() {
+function handleKeyDown(event) {
+  if (event.key === 'ArrowUp') {
+    handleJump();
+  }
+}
+
+function detectCollision() {
+  // プラットフォームとの衝突検出
   if (
-    playerX < obstacleX + obstacleSize &&
-    playerX + playerSize > obstacleX &&
-    playerY < obstacleY + obstacleSize &&
-    playerY + playerSize > obstacleY
+    mario.x < platform.x + platform.width &&
+    mario.x + mario.width > platform.x &&
+    mario.y + mario.height > platform.y
   ) {
-    return true; // 衝突した
+    mario.velocityY = 0;
+    mario.y = platform.y - mario.height;
+    mario.jumping = false;
   }
-  return false; // 衝突していない
-}
 
-// ゲームクリア判定
-function checkGameClear() {
+  // 敵キャラクターとの衝突検出
   if (
-    playerX < goalX + goalSize &&
-    playerX + playerSize > goalX &&
-    playerY < goalY + goalSize &&
-    playerY + playerSize > goalY
+    mario.x < enemy.x + enemy.width &&
+    mario.x + mario.width > enemy.x &&
+    mario.y < enemy.y + enemy.height &&
+    mario.y + mario.height > enemy.y
   ) {
-    return true; // ゲームクリア
+    // 衝突時の処理（ゲームオーバーなど）
+    console.log('ゲームオーバー');
   }
-  return false; // ゲームクリアしていない
+
+  // コインの収集判定
+  if (
+    mario.x < coin.x + coin.width &&
+    mario.x + mario.width > coin.x &&
+    mario.y < coin.y + coin.height &&
+    mario.y + mario.height > coin.y
+  ) {
+    if (!coin.collected) {
+      coin.collected = true;
+      console.log('コインを収集しました');
+    }
+  }
+
+  // ゴールとの衝突検出
+  if (
+    mario.x < goal.x + goal.width &&
+    mario.x + mario.width > goal.x &&
+    mario.y < goal.y + goal.height &&
+    mario.y + mario.height > goal.y
+  ) {
+    // ゴール到達時の処理（ゲームクリアなど）
+    console.log('ゲームクリア！');
+  }
 }
 
-// ゲームループ
+function updateEnemy() {
+  enemy.x -= enemy.speed;
+
+  if (enemy.x + enemy.width < 0) {
+    enemy.x = canvas.width;
+  }
+}
+
 function gameLoop() {
-  // キャンバスをクリア
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // プレイヤーを描画
-  ctx.fillRect(playerX, playerY, playerSize, playerSize);
+  drawMario();
+  drawPlatform();
+  drawEnemy();
+  drawCoin();
+  drawGoal();
+  applyGravity();
+  detectCollision();
+  updateEnemy();
 
-  // スタートを描画
-  ctx.fillStyle = 'green';
-  ctx.fillRect(startX, startY, playerSize, playerSize);
-
-  // ゴールを描画
-  ctx.fillStyle = 'red';
-  ctx.fillRect(goalX, goalY, goalSize, goalSize);
-
-  // 障害物を描画
-  ctx.fillStyle = 'gray';
-  ctx.fillRect(obstacleX, obstacleY, obstacleSize, obstacleSize);
-
-  // タッチの入力に応じてプレイヤーの位置を更新
-  for (let touchId in touches) {
-    let touch = touches[touchId];
-    if (touch.x < playerX && playerX > 0) {
-      playerX -= 5;
-    }
-    if (touch.x > playerX && playerX < canvas.width - playerSize) {
-      playerX += 5;
-    }
-    if (touch.y < playerY && playerY > 0) {
-      playerY -= 5;
-    }
-    if (touch.y > playerY && playerY < canvas.height - playerSize) {
-      playerY += 5;
-    }
-  }
-
-  // 衝突判定
-  if (checkCollision()) {
-    // ゲームオーバー処理
-    alert('ゲームオーバー');
-    resetGame();
-  } else if (checkGameClear()) {
-    // ゲームクリア処理
-    alert('ゲームクリア！');
-    resetGame();
-  } else {
-    // ループを継続するために再帰呼び出し
-    requestAnimationFrame(gameLoop);
-  }
+  window.requestAnimationFrame(gameLoop);
 }
 
-// ゲームをリセット
-function resetGame() {
-  playerX = startX;
-  playerY = startY;
-}
+window.addEventListener('keydown', handleKeyDown);
 
-// ゲームループを開始
 gameLoop();
